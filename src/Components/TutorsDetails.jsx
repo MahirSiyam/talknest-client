943dcb7d8feebc21f9bd9d50624277da9b262649
Mahir Siyam <mahirmubtasimsiyam@gmail.com>
2025-06-06 08:28:16 +0600

    add book now for database

--- src/Components/TutorsDetails.jsx
+++ src/Components/TutorsDetails.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { use } from "react";
 import { useLoaderData } from "react-router";
+import { AuthContext } from "../Provider/AuthProvider";
+import axios from "axios";
 
 const TutorsDetails = () => {
+    const {user} = use(AuthContext);
+    // console.log(user);
   const details = useLoaderData();
-  const { _id, name, image, language, description , price } = details;
+  const { _id, email , name, image, language, description , price } = details;
+
+//   console.log(details);
+
+    const handleBookNow = () => {
+
+        const booktutors = {
+            _id,
+            image,
+            language,
+            price,
+            tutorEmail: email,
+            user: user.email
+        }
+
+        axios.post('http://localhost:3000/bookTutors' , booktutors)
+        .then(res => {
+            console.log(res.data);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+
+    }
 
   return (
     <div className="flex-col md:flex lg:card-side gap-2 bg-base-100 shadow-lg m-[34px] border-2 border-gray-50 rounded-tr-4xl">
+        <title>Tutor Details</title>
       <figure>
         <img
           src={image}
@@ -19,7 +47,7 @@ const TutorsDetails = () => {
         <p className="text-lg font-semibold">Price: {price} $</p>
         <p className="text-lg font-extralight">{description}</p>
         <div className="card-actions">
-          <button className="btn bg-[#117a65] font-semibold text-white">Book Now</button>
+          <button onClick={handleBookNow} className="btn bg-[#117a65] font-semibold text-white">Book Now</button>
         </div>
       </div>
     </div>
--- src/Pages/FindTutors.jsx
+++ src/Pages/FindTutors.jsx
@@ -8,6 +8,7 @@ const FindTutors = () => {
 
     return (
         <div className='grid grid-cols-1 md:grid-cols-2 gap-5 w-11/12 mx-auto mt-2 mb-2 p-5'>
+            <title>Find Tutors</title>
             {
                 tutorials.map(tutorial => <TutorsCard key={tutorial._id} tutorial= {tutorial}></TutorsCard>)
             }
